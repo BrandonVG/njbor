e2f7cdf43d63cf72fa5bef4940671b01f3f23903
BrandonVG <bvadillo0@ucol.mx>
2021-05-25 23:52:52 -0500

    Si

---
+++ index.js
@@ -0,0 +1,4 @@
+const key = require("./key.js");
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+bot.login(key);
---
+++ key.js
@@ -0,0 +1,2 @@
+const key = "ODQ2OTcwOTMwNzA1NTk2NDI3.YK3RJQ.0B7Urh6hyi1ffFyakeX9QnF4ZA0";
+module.exports = key;
